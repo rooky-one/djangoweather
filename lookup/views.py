e8c2847858bcb6ed363904914c4e59a200761aa9
rooky-one <br-ian@hotmail.co.uk>
2023-07-19 00:25:06 +0100

    test...

--- lookup/views.py
+++ lookup/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+	# ApI URL goes here:
+	# http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/322690?res=3hourly&key=ca6ccf25-e065-494b-9f2d-13a38f584994
+
 	return render(request, 'home.html', {})
 
 def about(request):
