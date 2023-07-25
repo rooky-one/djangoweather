5be03368de49856ee8f5e470deba030d5666214f
rooky-one <br-ian@hotmail.co.uk>
2023-07-25 14:46:38 +0100

    Update 02 to API Calls

--- lookup/views.py
+++ lookup/views.py
@@ -19,6 +19,10 @@ def home(request):
     except Exception as e:
         api = "Error..."
 
+   # If api.SiteRep.DV.Location.Period.0.value = 7
+        # "Test"
+        # 00-03hrs < Type: {{ api.SiteRep.DV.Location.Period.0.Rep.0.W }} | Temp: {{ api.SiteRep.DV.Location.Period.0.Rep.0.T }}oC | Rain: {{ api.SiteRep.DV.Location.Period.0.Rep.0.Pp }}% | UV: {{ api.SiteRep.DV.Location.Period.0.Rep.0.U }}% ><br/>
+
     # API URL goes here:
     # http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/322690?res=3hourly&key=ca6ccf25-e065-494b-9f2d-13a38f584994
 
