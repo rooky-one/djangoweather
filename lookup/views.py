3279d6a8928a61698b11d25bdd388518e2a4f7b0
rooky-one <br-ian@hotmail.co.uk>
2023-07-25 18:44:32 +0100

    Testing Git via Pycharm

--- lookup/views.py
+++ lookup/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
-
-# Create your views here....
-
+# Create your views here...
 
 def home(request):
     import json
     import requests
+
     # Works 21/07/2023
     api_request = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/322690?res=3hourly&key=ca6ccf25-e065-494b-9f2d-13a38f584994")
-
-
 
     try:
         api = json.loads(api_request.content)
@@ -19,9 +16,9 @@ def home(request):
     except Exception as e:
         api = "Error..."
 
-   # If api.SiteRep.DV.Location.Period.0.value = 7
-        # "Test"
-        # 00-03hrs < Type: {{ api.SiteRep.DV.Location.Period.0.Rep.0.W }} | Temp: {{ api.SiteRep.DV.Location.Period.0.Rep.0.T }}oC | Rain: {{ api.SiteRep.DV.Location.Period.0.Rep.0.Pp }}% | UV: {{ api.SiteRep.DV.Location.Period.0.Rep.0.U }}% ><br/>
+    # If api.SiteRep.DV.Location.Period.0.value = 7
+
+    # 00-03hrs < Type: {{ api.SiteRep.DV.Location.Period.0.Rep.0.W }} | Temp: {{ api.SiteRep.DV.Location.Period.0.Rep.0.T }}oC | Rain: {{ api.SiteRep.DV.Location.Period.0.Rep.0.Pp }}% | UV: {{ api.SiteRep.DV.Location.Period.0.Rep.0.U }}% ><br/>
 
     # API URL goes here:
     # http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/322690?res=3hourly&key=ca6ccf25-e065-494b-9f2d-13a38f584994
@@ -29,7 +26,6 @@ def home(request):
     return render(request, 'home.html', {
         'api': api,
     })
-
 
 def about(request):
     return render(request, 'about.html', {})
